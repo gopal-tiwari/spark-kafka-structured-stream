9427ff4e962eb617af2eb79843280699754e90ad
gopal <gopal.tiwari2012@gmail.com>
2020-07-07 14:11:00 +0530

    hdfs sink method added

--- src/main/scala/org/connected/kafka/StreamReader.scala
+++ src/main/scala/org/connected/kafka/StreamReader.scala
@@ -1,6 +1,17 @@
 package org.connected.kafka
 
+import org.apache.spark.sql.DataFrame
+
 object StreamReader
 {
+
+  def kafkaHDFSSink(streamingDataFrame:DataFrame)={
+    streamingDataFrame
+      .writeStream
+      .format("parquet")
+      .option("path", "/tmp/stream_data")
+      .option("checkpointLocation","/tmp/stream_checkpoint")
+      .partitionBy("date", "hour", "minute")
+  }
 
 }
