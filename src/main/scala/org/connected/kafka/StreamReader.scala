2fd18aea59c0ad5a08aafbd79cb7257ac874cc99
gopal <gopal.tiwari2012@gmail.com>
2020-07-08 16:28:33 +0530

    col added

--- src/main/scala/org/connected/kafka/StreamReader.scala
+++ src/main/scala/org/connected/kafka/StreamReader.scala
@@ -1,16 +1,41 @@
 package org.connected.kafka
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 object StreamReader
 {
 
-  def kafkaHDFSSink(streamingDataFrame:DataFrame)={
+  def streamColumnMapping(kafkaStreamDataframe: DataFrame) =
+  {
+    val kafkaDataSchema = StructType(List(
+      StructField("dataTimestamp", StringType),
+      StructField("data", StringType)
+    ))
+
+    val streamingDataWithColumn = kafkaStreamDataframe.select(
+      col("timestamp").alias("kafka_timestamp"),
+      col("partition"),
+      col("offset"),
+      col("key"),
+      from_json(col("value").cast("String"), kafkaDataSchema ).getField("data").alias("event_data"),
+      from_json(col("value").cast("String"), kafkaDataSchema ).getField("dataTimestamp").cast("timestamp").alias("data_timestamp"),
+      date_format(col("timestamp"), "yyyy-MM-dd").alias("process_date"),
+      hour(col("timestamp")).alias("hour"),
+      minute(col("timestamp")).alias("minute")
+    )
+      .withColumn("new_col", col("event_data"))
+    streamingDataWithColumn
+  }
+
+  def kafkaHDFSSink(streamingDataFrame: DataFrame) =
+  {
     streamingDataFrame
       .writeStream
       .format("parquet")
       .option("path", "/tmp/stream_data")
-      .option("checkpointLocation","/tmp/stream_checkpoint")
+      .option("checkpointLocation", "/tmp/stream_checkpoint")
       .partitionBy("date", "hour", "minute")
   }
 
